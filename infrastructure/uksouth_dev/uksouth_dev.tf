7eadf4edc4b16977adfab8be0f7b823383c7d57d
Richard Slater <richard.slater@amido.com>
2018-03-09 23:21:28 +0000

    Add backend for `uksouth` :cloud:

---
+++ infrastructure/uksouth_dev/uksouth_dev.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "chatbot-170-tfstate-rg"
+    storage_account_name = "chatbot170tfstatestor"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
