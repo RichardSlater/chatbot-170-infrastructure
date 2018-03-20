32e2a4c446a0811b74b99302041a6ce46816276e
Richard Slater <github@richard-slater.co.uk>
2018-03-20 12:57:58 +0000

    Add QnA Bot :question:

--- bot/app.js
+++ bot/app.js
@@ -1,4 +1,6 @@
 import PerformanceManagement from './azure/PerformanceManagement.mjs'
+import CognitiveServices from 'botbuilder-cognitiveservices'
+
 const apm = new PerformanceManagement()
 
 const restify = require('restify')
@@ -7,13 +9,25 @@ const builder = require('botbuilder')
 const port = process.env.port || process.env.PORT || 3978
 
 // TODO: Move to Secret Management
-const appId = process.env.MicrosoftAppId
-const appPassword = process.env.MicrosoftAppPassword
+const appId = process.env.MSFT_APP_ID
+const appPassword = process.env.MSFT_APP_PASSWORD
+const qnaKnowledgeBaseId = process.env.QNA_KNOWLEDGE_BASE_ID
+const qnaSubscriptionId = process.env.QNA_SUBSCRIPTION_KEY
 // END TODO
 
 const server = restify.createServer()
 server.listen(port, function () {
   apm.serverStart(server.name, server.url)
+})
+
+const recognizer = new CognitiveServices.QnAMakerRecognizer({
+  knowledgeBaseId: qnaKnowledgeBaseId,
+  subscriptionKey: qnaSubscriptionId})
+
+const qnaDialog = new CognitiveServices.QnAMakerDialog({
+  recognizers: [recognizer],
+  defaultMessage: 'Sorry, I don\'t know how to answer that question, call us on 0800 555 555.',
+  qnaThreshold: 0.3
 })
 
 const connector = new builder.ChatConnector({
@@ -23,8 +37,8 @@ const connector = new builder.ChatConnector({
 
 server.post('/api/messages', connector.listen())
 
-const bot = new builder.UniversalBot(connector, function (session) {
-  const message = `You said: ${session.message.text}`
-  apm.traceBotMessage(message)
-  session.send(message)
-})
+const bot = new builder.UniversalBot(connector)
+
+// TODO: move to Table Storage backend.
+bot.set('storage', new builder.MemoryBotStorage())
+bot.dialog('/', qnaDialog)
