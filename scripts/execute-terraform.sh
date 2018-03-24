d3b715c867017d1ac6f096df7c46a894e76ec338
Richard Slater <github@richard-slater.co.uk>
2018-03-24 20:33:05 +0000

    Fix syntax of Terraform scripts. :balloon:

--- scripts/execute-terraform.sh
+++ scripts/execute-terraform.sh
@@ -1,9 +1,11 @@
-if [ $# -eq 0 ] then
-  echo "No arguments supplied, specify either 'apply' or 'destroy'."
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, specify either 'apply' or 'destroy'."
 fi
 
-if [ ($1 != "apply") && ($1 != "destroy") ] then
-  echo "Invalid argument supplied, specify either 'apply' or 'destroy'."
+if [ ($1 != "apply") && ($1 != "destroy") ]
+  then
+    echo "Invalid argument supplied, specify either 'apply' or 'destroy'."
 fi
 
 cd "$CHATBOT170INFRASTRUCTURE_MASTER_STATE"
@@ -11,10 +13,12 @@ terraform --version
 terraform init
 terraform get
 
-if [($1 == 'apply')] then
-  terraform apply --auto-approve
+if [($1 == 'apply')]
+  then
+    terraform apply --auto-approve
 fi
 
-if [($1 == 'destroy')] then
-  terraform destroy -force
+if [($1 == 'destroy')]
+  then
+    terraform destroy -force
 fi
